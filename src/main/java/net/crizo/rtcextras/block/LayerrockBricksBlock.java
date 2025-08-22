a2903fbc688727fc3c9a70cc6447f14fca480054
vkhsd <starf@DESKTOP-104CJQQ>
2025-08-22 15:20:17 -0500

    gleissa a a b etter !!!!!!

---
+++ src/main/java/net/crizo/rtcextras/block/LayerrockBricksBlock.java
@@ -0,0 +1,20 @@
+package net.crizo.rtcextras.block;
+
+import net.minecraft.world.level.block.state.properties.NoteBlockInstrument;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.core.BlockPos;
+
+public class LayerrockBricksBlock extends Block {
+	public LayerrockBricksBlock() {
+		super(BlockBehaviour.Properties.of().sound(SoundType.ANCIENT_DEBRIS).strength(3f, 25f).requiresCorrectToolForDrops().instrument(NoteBlockInstrument.BASEDRUM));
+	}
+
+	@Override
+	public int getLightBlock(BlockState state, BlockGetter worldIn, BlockPos pos) {
+		return 15;
+	}
+}
