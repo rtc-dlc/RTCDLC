b6abe50ae520a1732e2d0f7738d24bff6b5abc16
starf <starf@DESKTOP-104CJQQ>
2025-08-21 20:55:54 -0500

    MCreator Merge commit '8c990b341ff31ab3e933b92b05ab17d17060f854' into 83931a8009850366a8a6378ad441091a0e35f364

--- src/main/java/net/crizo/rtcextras/init/RtcExtrasModTabs.java
+++ src/main/java/net/crizo/rtcextras/init/RtcExtrasModTabs.java
@@ -5,20 +5,15 @@ package net.crizo.rtcextras.init;
 
 import net.neoforged.neoforge.registries.DeferredRegister;
 import net.neoforged.neoforge.registries.DeferredHolder;
-import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
-import net.neoforged.bus.api.SubscribeEvent;
 
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.network.chat.Component;
 import net.minecraft.core.registries.Registries;
 
 import net.crizo.rtcextras.RtcExtrasMod;
 
-@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public class RtcExtrasModTabs {
 	public static final DeferredRegister<CreativeModeTab> REGISTRY = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, RtcExtrasMod.MODID);
 	public static final DeferredHolder<CreativeModeTab, CreativeModeTab> RTC_EXTRAS = REGISTRY.register("rtc_extras",
@@ -33,14 +28,4 @@ public class RtcExtrasModTabs {
 				tabData.accept(RtcExtrasModBlocks.DIRTY_SHALE.get().asItem());
 				tabData.accept(RtcExtrasModBlocks.YELLOW_LAYERROCK.get().asItem());
 			}).withSearchBar().build());
-
-	@SubscribeEvent
-	public static void buildTabContentsVanilla(BuildCreativeModeTabContentsEvent tabData) {
-		if (tabData.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
-			tabData.accept(RtcExtrasModBlocks.LAYERROCK_ROOTS.get().asItem());
-			tabData.accept(RtcExtrasModBlocks.SHORT_LAYERROCK_ROOTS.get().asItem());
-			tabData.accept(RtcExtrasModBlocks.TALL_LAYERROCK_ROOTS.get().asItem());
-			tabData.accept(RtcExtrasModBlocks.BULBUSH.get().asItem());
-		}
-	}
 }
